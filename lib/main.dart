f66b3feb6f2f0c69fcd4cad54dd67d81be257eca
jeffersonamaral <jefferson.amarals@gmail.com>
2023-11-24 16:51:19 -0300

    Criação do projeto.

---
+++ lib/main.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MaterialApp(
+      title: 'Leitor de eBooks',
+      debugShowCheckedModeBanner: false,
+      home: Center(
+        child: Text('Texto'),
+      ))
+  );
+}
