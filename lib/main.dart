fee32a1a71b69ca4a1cdb12fadf0906e2babf51a
jeffersonamaral <jefferson.amarals@gmail.com>
2023-11-25 12:52:33 -0300

    Configuração das cores do Tema.

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,20 @@
 import 'package:flutter/material.dart';
 import 'package:ebook_reader/view/home.dart';
+import 'package:ebook_reader/util/utilities.dart';
 
 void main() {
+  toggleDebug(true);
+
   runApp(
       MaterialApp(
           debugShowCheckedModeBanner: false,
           title: 'Leitor de eBooks',
           theme: ThemeData(
-            primarySwatch: Colors.blue,
-            primaryColor: Colors.amber,
+            primaryColor: Colors.white,
+            primarySwatch: Colors.blueGrey,
+            textSelectionTheme: const TextSelectionThemeData(
+                selectionColor: Colors.blue
+            ),
           ),
           home: const Home()
       )
