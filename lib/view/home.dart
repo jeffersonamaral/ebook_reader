e145068b31f2ad87744f39a24819b83cbc9bfa0e
jeffersonamaral <jefferson.amarals@gmail.com>
2023-11-25 12:54:03 -0300

    Alteração para carregar os livros assincronamente a partir da API.

--- lib/view/home.dart
+++ lib/view/home.dart
@@ -1,6 +1,10 @@
 import 'package:async/async.dart';
 import 'package:flutter/material.dart';
 
+import '../model/book_model.dart';
+import '../controller/book_controller.dart';
+import 'widget/book_list_view.dart';
+
 class Home extends StatefulWidget {
   const Home({super.key});
 
@@ -10,36 +14,44 @@ class Home extends StatefulWidget {
 
 class _HomeState extends State<Home> with SingleTickerProviderStateMixin, AutomaticKeepAliveClientMixin {
 
+  String _title = 'Livros';
+
   late TabController _tabController;
 
   final AsyncMemoizer _memoizerBooks = AsyncMemoizer();
 
   final AsyncMemoizer _memoizerFavorities = AsyncMemoizer();
 
+  final BookController _bookController = BookController();
+
   @override
   bool get wantKeepAlive => true;
 
-  String _title = 'Livros';
+  List<BookModel> _books = [];
+
+  Future<dynamic> _loadBooks() async {
+    return _memoizerBooks.runOnce(() {
+      return _bookController.loadBooks();
+    });
+  }
 
   @override
   void initState() {
     super.initState();
 
-    _tabController = TabController(
-        length: 2,
-        vsync: this
-    )..addListener(() {
-      setState(() {
-        switch (_tabController.index) {
-          case 0:
-            _title = 'Livros';
-            break;
-          case 1:
-            _title = 'Favoritos';
-            break;
-        }
+    _tabController = TabController(length: 2, vsync: this)
+      ..addListener(() {
+        setState(() {
+          switch (_tabController.index) {
+            case 0:
+              _title = 'Livros';
+              break;
+            case 1:
+              _title = 'Favoritos';
+              break;
+          }
+        });
       });
-    });
   }
 
   @override
@@ -48,8 +60,8 @@ class _HomeState extends State<Home> with SingleTickerProviderStateMixin, Automa
 
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: Colors.blueAccent,
-        title: Text(_title),
+          backgroundColor: Colors.blueAccent,
+          title: Text(_title),
           bottom: PreferredSize(
               preferredSize: const Size(double.infinity, kToolbarHeight),
               child: Padding(
@@ -64,15 +76,55 @@ class _HomeState extends State<Home> with SingleTickerProviderStateMixin, Automa
                         ],
                       )
                     ],
-                  )
-              )
-          )
-      ),
+                  )))),
       body: TabBarView(
         controller: _tabController,
-        children: const [
-          Center(child: Text('Listagem de Livros')),
-          Center(child: Text('Listagem de Favoritos')),
+        children: [
+          FutureBuilder<dynamic>(
+              future: _loadBooks(),
+              builder: (context, snapshot) {
+                if (snapshot.connectionState == ConnectionState.none
+                    || snapshot.connectionState == ConnectionState.active
+                    || snapshot.connectionState == ConnectionState.waiting) {
+                  return const Center(
+                    child: CircularProgressIndicator(),
+                  );
+                } else {
+                  if (snapshot.hasData) {
+                    if (snapshot.hasError) {
+                      return Center(
+                        child: Text('Ocorreu um erro: ${snapshot.error}'),
+                      );
+                    } else {
+                      List<BookModel> books = snapshot.data!;
+
+                      /*
+                      books.forEach((element) {
+                        if (_favorites.contains(element)) {
+                          element.favorite = true;
+                        }
+                      });
+                      */
+
+                      return BookListView(snapshot.data, null /*_favorites*/, onFavorite: (model) {
+                        /*
+                        if (model.favorite) {
+                          _favoriteController.delete(model);
+                        } else {
+                          _favoriteController.save(model);
+                        }
+                        */
+                      });
+                    }
+                  } else {
+                    return const Center(
+                      child: Text('Nenhum dado foi encontrado.'),
+                    );
+                  }
+                }
+              }
+          ),
+          const Center(child: Text('Listagem de Favoritos')),
         ],
       ),
     );
